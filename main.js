0c132c008bec22fb08a2505a3ebecdd8eb7a3e8d
Rohit <rohit@Rohits-MacBook-Pro.local>
2017-03-06 17:38:42 +0530

    making async

--- main.js
+++ main.js
@@ -12,31 +12,36 @@ var server = http.createServer(function(req, res) {
     console.log('Listening at: http://localhost:' + port.toString());
 });
 
-function getLastLines (fName, lines) {
-  var fNameStat = fs.statSync(fName);
-  var startOffset = fNameStat.size
-  var endOffset = startOffset
-  var finalStartPosition = fNameStat.size
-  var fd = fs.openSync(fName, 'r')
-
-  while(true){
-    startOffset = Math.max(0, startOffset - 1024)
-    var dataLength = endOffset - startOffset
-    var buffer = new Buffer(dataLength);
-    fs.readSync(fd, buffer, 0, dataLength, startOffset)
-    var text = buffer.toString()
-    for(var i=text.length-1; i>=0; i--){
-      if(text[i]=='\n') lines--
-      if(lines==0) break;
-      finalStartPosition--
-    }
-    endOffset = startOffset
-    if(lines==0 || endOffset == 0) break
-  }
-  var buffer = new Buffer(fNameStat.size - finalStartPosition);
-  var newData = fs.readSync(fd, buffer, 0, fNameStat.size - finalStartPosition, finalStartPosition)
-  fs.closeSync(fd)
-  return buffer.toString()
+function getLastLinesPromisified(filename, lines) {
+  return new Promise(function (resolve, reject) {
+    var fNameStat = fs.statSync(fName);
+    var startOffset = fNameStat.size
+    var endOffset = startOffset
+    var finalStartPosition = fNameStat.size
+    fs.open(fName, 'r', (err, fd) => {
+      if(err) reject(err)
+      while(true){
+        startOffset = Math.max(0, startOffset - 1024)
+        var dataLength = endOffset - startOffset
+        var buffer = new Buffer(dataLength);
+        fs.readSync(fd, buffer, 0, dataLength, startOffset)
+        var text = buffer.toString()
+        for(var i=text.length-1; i>=0; i--){
+          if(text[i]=='\n') lines--
+          if(lines==0) break;
+          finalStartPosition--
+        }
+        endOffset = startOffset
+        if(lines==0 || endOffset == 0) break
+      }
+      var buffer = new Buffer(fNameStat.size - finalStartPosition);
+      
+      fs.read(fd, buffer, 0, fNameStat.size - finalStartPosition, finalStartPosition, function (err, bytesRead, completeData) {
+        if (err) reject(err)
+        resolve(completeData.toString())
+      })
+    });
+  });
 }
 
 socketio.listen(server).on('connection', function (socket) {
@@ -45,19 +50,23 @@ socketio.listen(server).on('connection', function (socket) {
     console.log(fName + ' is not a file');
     process.exit(1);
   }
-  socket.send(getLastLines(fName, 10))
-  fs.watch(fName, function (event, filename) {
-    var fNameStatChanged = fs.statSync(fName);
-    fs.open(fName, 'r', function(err, fd) {
-      var newDataLength = fNameStatChanged.size - fNameStat.size;
-      var buffer = new Buffer(newDataLength);
-      fs.read(fd, buffer, 0, newDataLength, fNameStat.size, function (err, bytesRead, newData) {
-         if (err) {
-            console.log(err);
-         };
-         socket.send(newData.toString())
+  getLastLinesPromisified(fName, 10)
+  .then(data => {
+    socket.send(data)
+    fs.watch(fName, function (event, filename) {
+      var fNameStatChanged = fs.statSync(fName);
+      fs.open(fName, 'r', function(err, fd) {
+        var newDataLength = fNameStatChanged.size - fNameStat.size;
+        var buffer = new Buffer(newDataLength);
+        fs.read(fd, buffer, 0, newDataLength, fNameStat.size, function (err, bytesRead, newData) {
+           if (err) console.log(err)
+           socket.send(newData.toString())
+        });
+        fNameStat = fs.statSync(fName)
       });
-      fNameStat = fs.statSync(fName);
     });
-  }); // fs.watch
+  })
+  .catch(error => {
+    console.log(error);
+  });
 });
